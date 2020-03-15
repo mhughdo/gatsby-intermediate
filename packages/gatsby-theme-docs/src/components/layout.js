7f40a4fc633203301fc6fb25c2144bf1b58a0656
Manh Hung <hungdm1211@gmail.com>
2020-03-15 17:19:11 +0700

    load MDX into GraphQL

---
+++ packages/gatsby-theme-docs/src/components/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Layout = ({children}) => (
+    <>
+        <header>gatsby-theme-docs</header>
+        <main>{children}</main>
+    </>
+)
+
+export default Layout
