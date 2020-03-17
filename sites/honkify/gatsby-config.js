4511f52ee52a49ef795ecbee09413e8e478d610f
Manh Hung <hungdm1211@gmail.com>
2020-03-17 15:36:02 +0700

    install theme

--- sites/honkify/gatsby-config.js
+++ sites/honkify/gatsby-config.js
@@ -1,14 +1,21 @@
 module.exports = {
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        defaultLayouts: {
-          default: require.resolve('./src/components/layout.js'),
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                defaultLayouts: {
+                    default: require.resolve('./src/components/layout.js'),
+                },
+            },
         },
-      },
-    },
-    'gatsby-plugin-theme-ui',
-  ],
-};
+        {
+            resolve: 'gatsby-theme-docs',
+            options: {
+                basePath: '/docs',
+                useExternalMDX: true,
+            },
+        },
+        'gatsby-plugin-theme-ui',
+    ],
+}
